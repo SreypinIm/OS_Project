4e37751d1159092ad8f0cef57fb9e8b89a395c44
SanaTes <zanates63@gmail.com>
2025-03-31 22:30:59 +0700

    Update count_files.sh

---
+++ OS_project/count_files.sh
@@ -0,0 +1,17 @@
+# Function to count files in a directory
+count_files() {
+  local directory="$1"
+
+  if [[ -z "$directory" ]]; then
+    read -p "Enter the directory path: " directory
+  fi
+
+  if [[ -d "$directory" ]]; then
+    local count=$(find "$directory" -type f | wc -l)
+    log_action "Counting files in $directory"
+    echo "Number of files in $directory: $count"
+  else
+    echo "Directory not found: $directory"
+    log_action "Directory not found: $directory"
+  fi
+}
