c5a93ffbcd6a67df472066c7b64276ec5b70c1ca
jessica2882 <kimj22676@gmail.com>
2025-04-01 16:14:38 +0700

    Update disk_usage.sh

---
+++ OS_project/disk_usage.sh
@@ -0,0 +1,16 @@
+# Function to display disk usage
+disk_usage() {
+  local directory="$1"
+
+  if [[ -z "$directory" ]]; then
+    read -p "Enter the directory path: " directory
+  fi
+
+  if [[ -d "$directory" ]]; then
+    log_action "Displaying disk usage of $directory"
+    du -sh "$directory"
+  else
+    echo "Directory not found: $directory"
+    log_action "Directory not found: $directory"
+  fi
+}
